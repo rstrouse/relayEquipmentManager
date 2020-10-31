bcf7ab5c7668c53ab001d951efbb432b2b850c15
Robert Strouse <rstrouse@truepointsolutions.com>
2020-10-31 13:24:26 -0700

    Still trying to get past the bs circular references

--- i2c-bus/I2cBus.ts
+++ i2c-bus/I2cBus.ts
@@ -474,7 +474,7 @@ export class i2cDeviceBase {
         try {
             let dt = dev.getDeviceType();
             if (typeof dt === 'undefined') return Promise.reject(new Error(`Cannot initialize I2c device id${dev.id} on Bus ${i2c.busNumber}: Device type not found ${dev.typeId}`));
-            let d = i2cDeviceFactory.createDevice(dt.deviceClass, i2c, dev);
+            let d = await i2cDeviceFactory.createDevice(dt.module, dt.deviceClass, i2c, dev);
             if (typeof d !== 'undefined') {
                 if (await d.initAsync(dt)) logger.info(`Device ${dt.name} initialized for i2c bus #${i2c.busNumber} address ${dev.address}`);
             }
--- i2c-bus/i2cFactory.ts
+++ i2c-bus/i2cFactory.ts
@@ -1,21 +1,11 @@
 ﻿import { logger } from "../logger/Logger";
-import { AtlasEZOorp, AtlasEZOpH, AtlasEZO } from "./AtlasEZO";
-import { I2cController, cont, I2cBus, I2cDevice, I2cDeviceFeed } from "../boards/Controller";
-
-interface II2cDevice {
-
-}
 export class i2cDeviceFactory {
     constructor() {
 
     }
-    public static createDevice(sName: string, i2c, dev) {
-        switch (sName) {
-            case 'AtlasEZOpH':
-                return new AtlasEZOpH(i2c, dev);
-            case 'AtlasEZOorp':
-                return new AtlasEZOorp(i2c, dev);
-        }
+    public static async createDevice(sModule: string, sName: string, i2c, dev): Promise<any> {
+        let mod = await import(sModule);
+        return Promise.resolve(new mod[sName](i2c, dev));
     }
 }
 
